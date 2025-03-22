0f56d0db40ed75aa934c558c88aad0bc382a45b1
Suyash29-source <suyash2v9@gmail.com>
2025-03-22 13:34:11 +0530

    Uploaded via Python script

---
+++ config.py
@@ -0,0 +1,6 @@
+# ✅ Store all bot settings & API keys here
+BOT_TOKEN = "7806071446:AAFukCv3jKDCM8cQKnk0UevHzGjCl5QD13E"
+CHANNEL_USERNAME = "chatbook29"
+APP_URL = "http://127.0.0.1:5000"  # Change this if deploying on Render
+SECRET_KEY = "supersecretkey"
+TELEGRAM_BOT_USERNAME = "Chatbookbot_bot"
